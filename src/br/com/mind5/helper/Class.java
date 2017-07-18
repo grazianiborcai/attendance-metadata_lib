60689938948046d2c1e3a3d520641aa57d6d0515
01547598 <01547598@BR1302986.valenet.valeglobal.net>
2017-07-17 22:26:53 -0300

    Phase01

--- src/br/com/mind5/helper/Class.java
+++ src/br/com/mind5/helper/Class.java
@@ -1,9 +1,16 @@
 package br.com.mind5.helper;
 
+import java.time.LocalTime;
+import java.util.ArrayList;
+
 public class Class {
 
 	private Long classID;
 	private Place place;
+	private LocalTime beginTime;
+	private Integer duration;
+	private LocalTime endTime;
+	private ArrayList<Weekday> weekday = new ArrayList<Weekday>();
 
 	public Long getClassID() {
 		return classID;
@@ -14,13 +21,52 @@ public class Class {
 	}
 
 	public Place getPlace() {
-		return place;
+		if (place != null)
+			return place;
+		else
+			return new Place();
 	}
 
 	public void setPlace(Place place) {
 		this.place = place;
 	}
 
+	public LocalTime getBeginTime() {
+		if (beginTime != null)
+			return beginTime;
+		else
+			return LocalTime.now();
+	}
 
+	public void setBeginTime(LocalTime beginTime) {
+		this.beginTime = beginTime;
+	}
+
+	public Integer getDuration() {
+		return duration;
+	}
+
+	public void setDuration(Integer duration) {
+		this.duration = duration;
+	}
+
+	public LocalTime getEndTime() {
+		if (endTime != null)
+			return endTime;
+		else
+			return LocalTime.now();
+	}
+
+	public void setEndTime(LocalTime endTime) {
+		this.endTime = endTime;
+	}
+
+	public ArrayList<Weekday> getWeekday() {
+		return weekday;
+	}
+
+	public void setWeekday(ArrayList<Weekday> weekday) {
+		this.weekday = weekday;
+	}
 
 }
